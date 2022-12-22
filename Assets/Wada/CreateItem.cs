c798584d6881caab4c7ae2d1d21708e105e5efee
wadashin <Shin.86.miso@outlook.com>
2022-12-22 13:49:00 +0900

    【追加】　ランダムな位置にランダムなアイテムが生成されるようにした　CreateItemクラスの追加をした

---
+++ Assets/Wada/CreateItem.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class CreateItem : MonoBehaviour
+{
+    [Tooltip("“¯Žž‚É¶¬‚·‚é”"), SerializeField]
+    int _synchroNum = 1;
+
+    [Tooltip("¶¬‚·‚éŠÔŠu"), SerializeField]
+    float _generateTime = 1f;
+
+    [Tooltip("¶¬‚·‚éêŠ"), SerializeField]
+    Transform[] _generatePos;
+
+
+
+    [Tooltip("ƒAƒCƒeƒ€"), SerializeField]
+    GameObject[] _scoreObj;
+
+    [SerializeField]
+    Items[] _items;
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private IEnumerator ItemGenerate()
+    {
+        yield return new WaitForSecondsRealtime(_generateTime);
+
+        int result = UnityEngine.Random.Range(0, _items.Sum(i => i.probability));
+
+        int sum = 0;
+
+        for (int i = 0; i < _items.Length; i++)
+        {
+            sum += _items[i].probability;
+            if (result <= sum)
+            {
+                ObjectPool.Instance.Use(_items[i].item.gameObject, _generatePos[UnityEngine.Random.Range(0, 3)].position);
+            }
+        }
+
+    }
+
+
+    [System.Serializable]
+    public class Items
+    {
+        public ItemBase item;
+        public int probability;
+    }
+}
--- Assets/Wada/TestObjectPool.cs
+++ Assets/Wada/TestObjectPool.cs
@@ -14,17 +14,17 @@ public class TestObjectPool : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(Random.Range(1, 101));
     }
 
     private IEnumerator CreateObj()
     {
         yield return new WaitForSecondsRealtime(1);
-        if(ObjectPool.Instance.GetCount(_box) < 10)
+        if(ObjectPool.Instance.GetCount(_box) < 40)
         {
             ObjectPool.Instance.Use(_box, this.transform.position);
         }
-        Debug.Log(1);
+        //Debug.Log(1);
         StartCoroutine(CreateObj());
     }
 }
