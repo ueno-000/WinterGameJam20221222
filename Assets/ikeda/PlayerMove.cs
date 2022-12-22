439005fecd05e3924a5ba1b93887428476646f5e
kaeru411r <ike269fr@gmail.com>
2022-12-22 12:24:27 +0900

    [update] プレイヤーの操作をできるように #1

--- Assets/ikeda/PlayerMove.cs
+++ Assets/ikeda/PlayerMove.cs
@@ -11,8 +11,19 @@ public class PlayerMove : MonoBehaviour
 {
     [Tooltip("移動速度")]
     [SerializeField] Vector2 _moveSpeed= Vector2.one;
+    [Header("移動キー")]
+    [Tooltip("右")]
+    [SerializeField] KeyCode _rightKey = KeyCode.D;
+    [Tooltip("左")]
+    [SerializeField] KeyCode _leftKey = KeyCode.A;
+    [Tooltip("上")]
+    [SerializeField] KeyCode _upKey = KeyCode.W;
+    [Tooltip("下")]
+    [SerializeField] KeyCode _downKey = KeyCode.S;
 
-    float _moveSpeedFact = 1;
+    [Header("デバッグ用 基本は1")]
+    [Tooltip("移動速度の倍率")]
+    [SerializeField] float _moveSpeedFact = 1;
     Rigidbody2D _rb;
 
     /// <summary>移動速度の倍率</summary>
@@ -21,13 +32,31 @@ public class PlayerMove : MonoBehaviour
     void Start()
     {
         _rb= GetComponent<Rigidbody2D>();
+        _rb.gravityScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 vec = new();
+        if (Input.GetKey(_rightKey))
+        {
+            vec.x++;
+        }
+        if(Input.GetKey(_leftKey))
+        {
+            vec.x--;
+        }
+        if (Input.GetKey(_upKey))
+        {
+            vec.y++;
+        }
+        if (Input.GetKey(_downKey))
+        {
+            vec.y--;
+        }
 
+        _rb.velocity = new Vector2(_moveSpeed.x * vec.x, _moveSpeed.y * vec.y) * _moveSpeedFact;
     }
 
 
