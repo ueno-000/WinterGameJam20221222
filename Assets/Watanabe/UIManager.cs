c906608c15597b10987f4649f3a2a87348ae00b1
Akira Watanabe <a0323w@gmail.com>
2022-12-22 15:24:52 +0900

    [add] Text表示用のクラス

---
+++ Assets/Watanabe/UIManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [Tooltip("シーン上のUI(0:Timer, 1:P1Score, 2:P2Score)")]
+    [SerializeField] private Text[] _texts = new Text[3];
+
+    private void Update()
+    {
+        if (_texts[0] != null)
+        {
+            //タイマー表示
+            _texts[0].text = GameManager.Timer.ToString("F2");
+        }
+        //スコア表示
+        _texts[1].text = GameManager.ScoreOne.ToString("F2");
+        _texts[2].text = GameManager.ScoreTwo.ToString("F2");
+    }
+}
